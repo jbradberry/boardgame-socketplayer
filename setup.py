bd7f2fc4238c0cbb6cea3ddb093e0356413f3bd5
Jeff Bradberry <jeff.bradberry@gmail.com>
2013-01-03 15:10:05 -0500

    Filled in a simple setup.py.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='BoardPlayer',
+    version='0.1dev',
+    author='Jeff Bradberry',
+    author_email='jeff.bradberry@gmail.com',
+    packages=['boardplayer'],
+    license='LICENSE',
+    description="A generic board game player.",
+)
