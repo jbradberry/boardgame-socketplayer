e17d725c4f3fcd9de4f6755b9b71db97eb31b6ef
Jeff Bradberry <jeff.bradberry@gmail.com>
2015-11-11 19:26:44 -0500

    Changed board-play.py to use argparse, and pass optional kwargs to the player object.

--- bin/board-play.py
+++ bin/board-play.py
@@ -1,5 +1,8 @@
 import sys
+import argparse
+
 from pkg_resources import iter_entry_points
+
 from boardplayer import player
 
 
@@ -13,18 +16,21 @@ player_plugins = dict(
     for ep in iter_entry_points('jrb_board.players')
 )
 
+parser = argparse.ArgumentParser(
+    description="Play a boardgame using a specified player type.")
+parser.add_argument('game', choices=sorted(board_plugins))
+parser.add_argument('player', choices=sorted(player_plugins))
+parser.add_argument('address', nargs='?')
+parser.add_argument('port', nargs='?', type=int)
+parser.add_argument('-e', '--extra', action='append')
 
-args = sys.argv[1:]
-addr, port = None, None
+args = parser.parse_args()
 
-board = board_plugins[args[0]]
-player_obj = player_plugins[args[1]]
-
-if len(args) > 2:
-    addr = args[2]
-if len(args) > 3:
-    port = int(args[3])
+board = board_plugins[args.game]
+player_obj = player_plugins[args.player]
+player_kwargs = dict(arg.split('=') for arg in args.extra or ())
 
 
-client = player.Client(player_obj(board()), addr, port)
+client = player.Client(player_obj(board(), **player_kwargs),
+                       args.address, args.port)
 client.run()
