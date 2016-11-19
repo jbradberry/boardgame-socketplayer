b030155e4b8de30078a16a91f0becd658d308cdb
Jeff Bradberry <jeff.bradberry@gmail.com>
2016-11-19 11:50:51 -0500

    Send and accept unpacked json representations over the wire
    This protocol will be easier to deal with by 3rd-party
    implementations, and will be more human-readable.

--- boardplayer/player.py
+++ boardplayer/player.py
@@ -1,6 +1,6 @@
-import sys
-import ast
+import json
 import socket
+import sys
 
 
 class Client(object):
@@ -11,9 +11,7 @@ class Client(object):
                          'decline': self.handle_decline,
                          'error': self.handle_error,
                          'illegal': self.handle_illegal,
-                         'update': self.handle_update,
-                         'action': self.handle_action,
-                         'winners': self.handle_winners,}
+                         'update': self.handle_update}
 
         self.addr = addr if addr is not None else '127.0.0.1'
         self.port = port if port is not None else 4242
@@ -25,63 +23,61 @@ class Client(object):
             message = self.socket.recv(4096)
             messages = message.rstrip().split('\r\n')
             for message in messages:
-                for action in self.receiver:
-                    if message.startswith(action):
-                        msg_contents = message[len(action)+1:]
-                        if msg_contents:
-                            msg_contents = ast.literal_eval(msg_contents)
-                        self.receiver[action](msg_contents)
-                        break
-                else:
+                data = json.loads(message)
+                if data['type'] not in self.receiver:
                     raise ValueError(
                         "Unexpected message from server: {0!r}".format(message))
 
-    def parse_item(self, item):
-        result = ast.literal_eval(item)
-        return result
+                self.receiver[data['type']](data)
 
-    def handle_player(self, msg):
-        print "You are player #{0}.".format(msg)
-        self.player.player = msg
+    def handle_player(self, data):
+        player = data['message']
+        print "You are player #{0}.".format(player)
+        self.player.player = player
 
-    def handle_decline(self, msg):
-        print msg
+    def handle_decline(self, data):
+        print data['message']
         self.running = False
 
-    def handle_error(self, msg):
-        print msg # FIXME: do something useful
+    def handle_error(self, data):
+        print data['message'] # FIXME: do something useful
 
-    def handle_illegal(self, msg):
-        print msg # FIXME: do something useful
+    def handle_illegal(self, data):
+        print data['message'] # FIXME: do something useful
 
-    def handle_update(self, msg):
-        action, state = msg
+    def handle_update(self, data):
+        state = data['state']
+        action = data.get('last_action', {}).get('notation') or ''
         self.player.update(state)
+
         print self.player.display(state, action)
+        if data.get('winners') is not None:
+            print self.player.winner_message(data['winners'])
+            self.running = False
+        elif data['state']['player'] == self.player.player:
+            action = self.player.get_action()
+            self.send({'type': 'action', 'message': action})
 
-    def handle_action(self, msg):
-        action = self.player.get_action()
-        self.socket.sendall("play {0!r}\r\n".format(action))
-
-    def handle_winners(self, msg):
-        print self.player.winner_message(msg)
-        self.running = False
+    def send(self, data):
+        self.socket.sendall("{0}\r\n".format(json.dumps(data)))
 
 
 class HumanPlayer(object):
     def __init__(self, board):
         self.board = board
         self.player = None
-        self.states = []
+        self.history = []
 
     def update(self, state):
-        self.states.append(state)
+        self.history.append(self.board.pack_state(state))
 
     def display(self, state, action):
+        state = self.board.pack_state(state)
+        action = self.board.pack_action(action)
         return self.board.display(state, action)
 
-    def winner_message(self, msg):
-        return self.board.winner_message(msg)
+    def winner_message(self, winners):
+        return self.board.winner_message(winners)
 
     def get_action(self):
         while True:
@@ -89,6 +85,6 @@ class HumanPlayer(object):
             action = self.board.pack_action(notation)
             if action is None:
                 continue
-            if self.board.is_legal(self.states, action):
+            if self.board.is_legal(self.history, action):
                 break
-        return action
+        return notation
